abb0165d11fce4f9130c75f4c4f6d9678fa84411
bgrauches <57674460+bgrauches@users.noreply.github.com>
2020-01-29 20:58:04 +0100

    Mira lo del Plus i falta baixar marxa

--- PRU03E02_Bartomeu_Grauches/src/pru03/E02/Cotxe.java
+++ PRU03E02_Bartomeu_Grauches/src/pru03/E02/Cotxe.java
@@ -19,6 +19,27 @@ public class Cotxe extends PRU03E02Cotxe_Bartomeu_Grauches {
 	public void setmManual(CanviarMarxaManual mManual) {
 		this.mManual = mManual;
 	}
+	
+	//Getters i setters marca, model i tipus de canvi
+		public String getMarca() {
+			return marca;
+		}
+		public void setMarca(String marca) {
+			this.marca = marca;
+		}
+		public String getModel() {
+			return model;
+		}
+		public void setModel(String model) {
+			this.model = model;
+		}
+		public TipusCanvi getTipuscanvi() {
+			return tipuscanvi;
+		}
+		public void setTipuscanvi(TipusCanvi tipuscanvi) {
+			this.tipuscanvi = tipuscanvi;
+		}
+		
 
 
 	public Cotxe(String marca, String model, TipusCanvi tipuscanvi) {
@@ -30,11 +51,32 @@ public class Cotxe extends PRU03E02Cotxe_Bartomeu_Grauches {
 		
 	}
 
-	public void CanviarMarxManual() {
-		
+	public void CanviarMarxaManual(char plus) throws Exception {
+		if(this.getTipuscanvi() == TipusCanvi.CanviManual) {
+			if (this.getmManual() == CanviarMarxaManual.N) {
+				this.mManual = CanviarMarxaManual.Primera;
+			}
+			else if (this.getmManual() == CanviarMarxaManual.Primera) {
+				this.mManual = CanviarMarxaManual.Segona;
+			}
+			else if (this.getmManual() == CanviarMarxaManual.Tercera) {
+				this.mManual = CanviarMarxaManual.Quarta;
+			}
+			else if (this.getmManual() == CanviarMarxaManual.Quarta) {
+				this.mManual = CanviarMarxaManual.Cinquena;
+			}
+			else if (this.getmManual() == CanviarMarxaManual.Cinquena) {
+				this.mManual = CanviarMarxaManual.Sisena;
+			}
+			
+			else if (this.getmManual() == CanviarMarxaManual.Sisena) {
+				throw new Exception("No hi ha sèptima marxa.");
+			}
+		}
 		
 		
 	}
+	
 	
 	public void CanviarMarxaAutomatic() {
 		
