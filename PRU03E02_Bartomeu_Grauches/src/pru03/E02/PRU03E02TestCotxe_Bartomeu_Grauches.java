5a8176828699a7a2c9e64e393617ba3b94a2f824
bgrauches <57674460+bgrauches@users.noreply.github.com>
2020-02-06 12:13:04 +0100

    Update PRU03E02TestCotxe_Bartomeu_Grauches.java

--- PRU03E02_Bartomeu_Grauches/src/pru03/E02/PRU03E02TestCotxe_Bartomeu_Grauches.java
+++ PRU03E02_Bartomeu_Grauches/src/pru03/E02/PRU03E02TestCotxe_Bartomeu_Grauches.java
@@ -1,6 +1,6 @@
 package pru03.E02;
 
-public class PRU03E02TestCotxe_Bartomeu_Grauches {
+public class PRU03E03TestCotxe_Bartomeu_Grauches {
 
 
 	public static void main(String[] args) {
@@ -8,13 +8,13 @@ public class PRU03E02TestCotxe_Bartomeu_Grauches {
 		//DECLARAM MARCA MODEL I CANVI DEL COTXU
 		PRU03E02Cotxe_Bartomeu_Grauches cotxu1 = new PRU03E02Cotxe_Bartomeu_Grauches ("Audi", "A3", TipusCanvi.CanviManual);
 		
-		//MËtode d'excepcions per arrancar motor
+		//M√®tode d'excepcions per arrancar motor
 		cotxu1.provaArrancarMotor();
 		
-		//MËtode per comprovar motor
-		System.out.println ("El motor est· " + cotxu1.comprovaMotor());
+		//M√®tode per comprovar motor
+		System.out.println ("El motor est√° " + cotxu1.comprovaMotor());
 		
-		//MËtode per comprovar revolucions
+		//M√®tode per comprovar revolucions
 		System.out.println ("Les revolucions son: " + cotxu1.getRevolucions());
 		
 		//Excepcions per comprovar si el cotxe esta aturat
